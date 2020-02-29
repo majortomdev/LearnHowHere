7e57f58627942e5b60dfff4b7f6e1a5cfd080149
Joseph Kinahan <kinahanj25@gmail.com>
2020-02-29 16:31:33 +0000

    added codes to courses

--- src/app/course-list/course-list.component.ts
+++ src/app/course-list/course-list.component.ts
@@ -10,12 +10,12 @@ import { Course } from './course';
 export class CourseListComponent implements OnInit {
 
   courseList: Course[] = [
-    new Course('Web Development', 'J.Hawkins', 'LHH cert A', '6 Weeks', 'None', '$375'),
-    new Course('SCSS and Styling', 'B.Jones', 'LHH cert A1', '4 Weeks', 'HTML', '$275'),
-    new Course('Java Foundations', 'T.Simpson', 'OCF', '8 Months', 'Basic IT', '$975'),
-    new Course('Java Associate', 'T.Simpson', 'OCA', '9 Months', 'Java Foundations', '$975'),
-    new Course('Java Professional', 'M.Stonehouse', 'OCP', '6 Months', 'Java Associate', '$1175'),
-    new Course('Python', 'J.Hawkins', 'LHH P+', '12 Weeks', 'Some porgramming', '$1075'),
+    new Course('Web Development', 'J.Hawkins', 'LHH cert A', '6 Weeks', 'None', '£375', 'W151'),
+    new Course('SCSS and Styling', 'B.Jones', 'LHH cert A1', '4 Weeks', 'HTML', '£275', 'W160'),
+    new Course('Java Foundations', 'T.Simpson', 'OCF', '8 Months', 'Basic IT', '£975', 'JA01'),
+    new Course('Java Associate', 'T.Simpson', 'OCA', '9 Months', 'Java Foundations', '£975', 'JA02'),
+    new Course('Java Professional', 'M.Stonehouse', 'OCP', '6 Months', 'Java Associate', '£1175', 'JA03'),
+    new Course('Python', 'J.Hawkins', 'LHH P+', '12 Weeks', 'Some porgramming', '£1075', 'P110'),
 
   ];
 
