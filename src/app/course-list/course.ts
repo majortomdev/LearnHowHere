453d2f745077cd2b08a275c7e3951b39fbdfb829
Joseph Kinahan <kinahanj25@gmail.com>
2020-02-29 15:04:07 +0000

    add course-list component & Course blueprint/class

---
+++ src/app/course-list/course.ts
@@ -0,0 +1,7 @@
+export class Course {
+
+  constructor(public courseName: string, public tutor: string, public certification: string,
+              public duration: string, public prereqs: string, public price: string ) {
+
+              }
+}
