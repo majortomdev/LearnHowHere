ee5057e8e1302b0c1da78d0fcd011973a8d6a88d
Joseph Kinahan <kinahanj25@gmail.com>
2020-03-02 17:22:18 +0000

    springBoot server added to project, + some tinkering courselist comp

---
+++ backend/learn-how-here/src/main/java/com/majortomdev/learnhowhere/LearnHowHereApplication.java
@@ -0,0 +1,13 @@
+package com.majortomdev.learnhowhere;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnHowHereApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearnHowHereApplication.class, args);
+	}
+
+}
---
+++ backend/learn-how-here/src/test/java/com/majortomdev/learnhowhere/LearnHowHereApplicationTests.java
@@ -0,0 +1,13 @@
+package com.majortomdev.learnhowhere;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnHowHereApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
